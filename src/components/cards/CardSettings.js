7692f1c42c13b29650a57d3fd7530b1903df8b3c
canarydev41 <mohawadkh41@gmail.com>
2022-07-05 23:37:00 +0300

    add dashboard screenshot

--- src/components/cards/CardSettings.js
+++ src/components/cards/CardSettings.js
@@ -5,81 +5,88 @@ import React from "react";
 export default function CardSettings() {
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-        <div className="rounded-t bg-white mb-0 px-6 py-6">
-          <div className="text-center flex justify-between">
-            <h6 className="text-blueGray-700 text-xl font-bold">My account</h6>
+      <div className="relative flex flex-col w-full min-w-0 mb-6 break-words border-0 rounded-lg shaodow-lg bg-blueGray-100">
+        <div className="px-6 py-6 mb-0 rounded-t bg-whkikte">
+          <div className="flex justify-between text-center">
+            <h6 className="text-xl font-bold text-blueGray-700">My account</h6>
             <button
-              className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+              className="px-4 py-2 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 rounded shadow outline-none active:bg-blue-600 hover:shadow-md focus:outline-none"
               type="button"
             >
               Settings
             </button>
           </div>
         </div>
-        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+        <div className="flex-auto px-4 py-10 pt-0 bg-graly-50 lg:px-10">
           <form>
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+            <h6 className="mt-3 mb-6 text-sm font-bold uppercase text-blueGray-400">
               User Information
             </h6>
             <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4">
+              <div className="w-full px-4 lg:w-6/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Username
                   </label>
+                  <div className="flex flex-row items-center justify-center bg-white border-0 rounded shadow">
+                    <span className="px-2 py-3 border-r rounded-l bg-gray-50">
+                      
+
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-6 stroke-blueGray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+</svg>
+                    </span>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="lucky.jesse"
+                    className="w-full py-3 text-sm transition-all duration-150 ease-linear bg-gray-50 placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
+                  </div>
+
                 </div>
               </div>
-              <div className="w-full lg:w-6/12 px-4">
+              <div className="w-full px-4 lg:w-6/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Email address
                   </label>
                   <input
                     type="email"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                     defaultValue="jesse@example.com"
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-6/12 px-4">
+              <div className="w-full px-4 lg:w-6/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     First Name
                   </label>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Lucky"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-6/12 px-4">
+              <div className="w-full px-4 lg:w-6/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Last Name
                   </label>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Jesse"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
@@ -87,67 +94,63 @@ export default function CardSettings() {
 
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+            <h6 className="mt-3 mb-6 text-sm font-bold uppercase text-blueGray-400">
               Contact Information
             </h6>
             <div className="flex flex-wrap">
-              <div className="w-full lg:w-12/12 px-4">
+              <div className="w-full px-4 lg:w-12/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Address
                   </label>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full px-4 lg:w-4/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     City
                   </label>
                   <input
                     type="email"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="New York"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full px-4 lg:w-4/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Country
                   </label>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="United States"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full px-4 lg:w-4/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     Postal Code
                   </label>
                   <input
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Postal Code"
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
@@ -155,22 +158,21 @@ export default function CardSettings() {
 
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
-            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+            <h6 className="mt-3 mb-6 text-sm font-bold uppercase text-blueGray-400">
               About Me
             </h6>
             <div className="flex flex-wrap">
-              <div className="w-full lg:w-12/12 px-4">
+              <div className="w-full px-4 lg:w-12/12">
                 <div className="relative w-full mb-3">
                   <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                     htmlFor="grid-password"
                   >
                     About me
                   </label>
                   <textarea
                     type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="A beautiful UI Kit and Admin for React & Tailwind CSS. It is Free and Open Source."
+                    className="w-full px-3 py-3 text-sm transition-all duration-150 ease-linear bg-white border-0 rounded shadow placeholder-blueGray-300 text-blueGray-600 focus:outline-none focus:ring"
                     rows="4"
                   ></textarea>
                 </div>
--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="text-shark-700 dark:text-shark-200 bg-white dark:bg-shark-800 body-font relative bottom-0 hero">
+    <footer className="relative bottom-0 bg-white text-shark-700 dark:text-shark-200 dark:bg-shark-800 body-font hero">
       <div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row ">
         <a
           href="/"
-          className="text-xl font-black leading-none text-shark-400 hover:text-shark-500 select-none "
+          className="text-xl font-black leading-none select-none text-shark-400 hover:text-shark-500 "
         >
-          <button className="flex justify-center items-center">
+          <button className="flex items-center justify-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-10 h-10 text-shark-500 dark:text-shark-400 mr-2"
+              className="w-10 h-10 mr-2 text-shark-500 dark:text-shark-400"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
             </svg>
-            <span className="ml-2 hidden lg:block text-shark-500 font-bold text-lg uppercase dark:text-shark-400">HK Dashbord</span>
+            <span className="hidden ml-2 text-lg font-bold uppercase lg:block text-shark-500 dark:text-shark-400">HK Dashbord</span>
           </button>
         </a>
         <span className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
